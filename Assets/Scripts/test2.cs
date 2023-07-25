223335ee4977de6917933f9c248686e7fefe7b5f
MingZhe Zhang <mingzzhang1@student.unimelb.edu.au>
2023-07-26 06:51:35 +0800

    Slider
    Implemented a slider for the siliderVal attribute in the script

---
+++ Assets/Scripts/test2.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test2 : MonoBehaviour
+{
+    public int interger = 100;
+    [SerializeField][Range(0, 100)] public int sliderVal = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
